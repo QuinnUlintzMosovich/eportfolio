38eec02f36631a79cca897c5a8610e2a021c3dc8
Quinn Ulintz Mosovich <quinn.ulintz.mosovich@esums.tech>
2025-02-20 15:40:02 +0000

    revisedLinks

--- core/views.py
+++ core/views.py
@@ -8,9 +8,9 @@ def hellotime(request):
     return HttpResponse(f"<h1>Hello, world!</h1> <p>It's {now}.</p>")
 
 def screenprint(request):
-    return render(request, "core/index1.html")
+    return render(request, "core/blackbox.html")
 def blackbox(request):
-    return render(request, "core/index.html")
+    return render(request, "core/screenprint.html")
 def aboutMe(request):
     return render(request, "core/about me.html")
 # Create your views here.
